4e03226fd8d624f5f079c40f7e2af729be264671
Will Madison <wmadisondev@gmail.com>
2021-03-27 08:38:24 -0400

    adds a default explorer when missing GITHUB_ACCESS_TOKEN handy for allowing the help to be seen without a token

--- cmd/explore/main.go
+++ cmd/explore/main.go
@@ -1,10 +1,13 @@
 package main
 
 import (
+	"fmt"
 	"os"
 
+	"github.com/willmadison/ossexplorer"
 	"github.com/willmadison/ossexplorer/cli"
 	"github.com/willmadison/ossexplorer/github"
+	"github.com/willmadison/ossexplorer/mocks"
 )
 
 func main() {
@@ -16,10 +19,15 @@ func main() {
 
 	accessToken := os.Getenv("GITHUB_ACCESS_TOKEN")
 
+	var explorer ossexplorer.Explorer
+
 	if accessToken == "" {
-		panic("missing GITHUB_ACCESS_TOKEN. Please ensure its appropriately set in your environment.")
+		fmt.Fprintf(env.Stdout, "warning: no GITHUB_ACCESS_TOKEN found defaulting to an in memory OSS explorer.\n")
+		org := ossexplorer.Organization{Name: "InMemoryFakeOrg"}
+		explorer = mocks.NewStubExplorer(org, nil)
+	} else {
+		explorer = github.NewExplorer(accessToken)
 	}
 
-	explorer := github.NewExplorer(accessToken)
 	os.Exit(cli.Run(explorer, env))
 }
